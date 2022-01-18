a65306f9ccc276484d69d567d6ed51738f5e2ff9
Are Edvardsen <biorakel@gmail.com>
2022-01-18 10:33:21 +0000

    actual var names

--- R/app_server.R
+++ R/app_server.R
@@ -10,24 +10,24 @@
 app_server <- function(input, output, session) {
 
   output$username <- shiny::renderUI(
-    paste("Brukernavn (SP_USERNAME):", Sys.getenv("USER"))
+    paste("Brukernavn (SP_USERNAME):", Sys.getenv("SP_USERNAME"))
   )
   output$usergroups <- shiny::renderUI(
-    paste("Brukergrupper (SP_USERGROUPS):", Sys.getenv("USER"))
+    paste("Brukergrupper (SP_USERGROUPS):", Sys.getenv("SP_USERGROUPS"))
   )
   output$userorgid <- shiny::renderUI(
-    paste("Brukerorganisasjon (SP_USERORGID):", Sys.getenv("USER"))
+    paste("Brukerorganisasjon (SP_USERORGID):", Sys.getenv("SP_USERORGID"))
   )
   output$useremail <- shiny::renderUI(
-    paste("Bruker epost (SP_USEREMAIL):", Sys.getenv("USER"))
+    paste("Bruker epost (SP_USEREMAIL):", Sys.getenv("SP_USEREMAIL"))
   )
   output$userphone <- shiny::renderUI(
-    paste("Bruker telefon (SP_USERPHONE):", Sys.getenv("USER"))
+    paste("Bruker telefon (SP_USERPHONE):", Sys.getenv("SP_USERPHONE"))
   )
   output$userfullname <- shiny::renderUI(
-    paste("Bruker fult navn (SP_USERFULLNAME):", Sys.getenv("USER"))
+    paste("Bruker fult navn (SP_USERFULLNAME):", Sys.getenv("SP_USERFULLNAME"))
   )
   output$userfullname <- shiny::renderUI(
-    paste("Bruker register (SP_USERREGID):", Sys.getenv("USER"))
+    paste("Bruker register (SP_USERREGID):", Sys.getenv("SP_USERREGID"))
   )
 }
